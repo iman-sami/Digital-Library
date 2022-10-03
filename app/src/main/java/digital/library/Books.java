bbedae5d3615079b3e8c8302302ae85acadcdc9a
iman-sami <imansamifaith@gmail.com>
2022-10-04 00:52:08 +0300

    update 2

---
+++ app/src/main/java/digital/library/Books.java
@@ -0,0 +1,12 @@
+package main.java.digital.library;
+
+import java.awt.*;
+import javax.*;
+public class Books {
+    JFrame frame;
+    public Books(){
+      frame = new JFrame();
+      frame.setSize(800, 600);
+    
+    }
+}
